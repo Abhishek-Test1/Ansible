cc6bcd49e46a3648cafcba02d6922804ef3c3c2c
Abhishek-Test1 <banerjee.abhishek2k@gmail.com>
2025-04-17 18:42:53 +0530

    Create topout.sh

---
+++ topout.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Define the output file
+OUTPUT_FILE="/home/ubuntu/scripts/top_output.txt"
+
+echo "Top command output has been saved from `hostname` on `date`" > $OUTPUT_FILE
+
+# Capture the top command output
+top -b -n 1 >> "$OUTPUT_FILE"
